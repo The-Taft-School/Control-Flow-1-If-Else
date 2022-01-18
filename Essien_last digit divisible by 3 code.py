617eed50779bbb9d0c6bb1a5e8fd8e37d351de90
Iyene <94707928+Iyene@users.noreply.github.com>
2022-01-18 13:25:41 -0500

    Update Essien_last digit divisible by 3 code.py

--- Essien_last digit divisible by 3 code.py
+++ Essien_last digit divisible by 3 code.py
@@ -2,6 +2,6 @@ number =int(input("Enter a number of your choice: "))
 
 last_digit = number % 10
 if(last_digit % 3 == 0 ):
-    print("{} is divisible by 3 ".format(last_digit))
+    print(number," is divisible by 3 ")
 else:
-    print("{} is not divisible by 3".format(last_digit))
+    print(number," is not divisible by 3")
