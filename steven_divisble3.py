47f8af5832e2efa7101483a4b6cc06091fc743c3
stevennnik <97125159+stevennnik@users.noreply.github.com>
2022-01-20 10:21:45 -0500

    hw4

---
+++ steven_divisble3.py
@@ -0,0 +1,7 @@
+a = int(input("give me a integer")
+b = a % 10
+
+if b == 3: print ("the last digit of {a} is divisible by 3")
+elif b == 6: print ("the last digit of {a} is divisible by 3")
+elif b == 9: print("the last digit of {a} is divisible by 3")
+else print("the last digit of {a} is not divisible by 3")
